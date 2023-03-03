69c2d88878e356585d859bfd3c7e0a6e2a295cd8
sreerampeela <59723039+sreerampeela@users.noreply.github.com>
2023-03-03 11:38:24 +0530

    Add files via upload

---
+++ write_edgelist.py
@@ -0,0 +1,19 @@
+import networkx as nx
+
+G = nx.read_graphml("string_spn_largecc.graphml",edge_key_type=int)
+
+p = nx.to_pandas_edgelist(G)
+
+sources = [i.split("(")[0].rstrip() for i in p["shared name"]]
+
+targets = [ i.split(")")[-1].rstrip() for i in p["shared name"]]
+
+fout = open("spn_largecc_edgelist.tab",'a',newline='\n',encoding='utf-8')
+for i in range(len(sources)):
+    edge = "\t".join([sources[i],targets[i]])
+    print(edge)
+    fout.write(edge+"\n")
+
+
+
+fout.close()
